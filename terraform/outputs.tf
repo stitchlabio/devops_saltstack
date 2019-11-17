cec74a7af0c8ac8657f515ba0e1428015cca565b
kc <kcfigaro@gmail.com>
2019-11-17 18:56:54 +0000

    added output - salt minion internal ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,10 @@ output "salt_minion_server_ip" {
   value = "${aws_instance.devops_minion_server.public_ip}"
 }
 
+output "salt_minion_internal_ip" {
+  value = "${aws_instance.devops_minion_server.private_ip}"
+}
+
 output "salt_master_log_in" {
   value = "ssh -i ~/environment/${var.key_name}.pem ubuntu@${aws_instance.devops_salt_master.public_ip}"
 }
